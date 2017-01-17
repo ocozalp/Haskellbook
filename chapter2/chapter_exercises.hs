1f395f92f6d5852cb44bccdb1da05afeefa417d2
ocozalp <ozalp.orhan@gmail.com>
2017-01-17 10:50:43 -0500

    Chapter 2 : Chapter Exercises

---
+++ chapter2/chapter_exercises.hs
@@ -0,0 +1,18 @@
+z = 7
+y = z + 8
+x = y ^ 2
+waxOn = x * 5
+
+-- Q1
+func1 = 10 + waxOn
+func2 = (+10) waxOn
+func3 = (-) 15 waxOn
+func4 = (-) waxOn 15
+
+-- Q2
+triple x = x * 3
+
+-- Q3
+func5 = triple waxOn
+
+
