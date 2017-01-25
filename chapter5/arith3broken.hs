9c90955f16ecac7ee75be203db90301a38c60937
ocozalp <ozalp.orhan@gmail.com>
2017-01-25 16:00:20 -0500

    Chapter 5 : Exercises

---
+++ chapter5/arith3broken.hs
@@ -0,0 +1,10 @@
+module Arith3Broken where
+
+main :: IO ()
+
+main = do
+    print (show $ 1 + 2)
+    putStrLn "10"
+    print (show $ negate $ -1)
+    print ((+) 0 blah)
+    where blah = negate 1
